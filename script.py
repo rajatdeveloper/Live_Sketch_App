1f60f15197ec84bec46d49b9b162316eb14a0ac7
Rajat Singhal <33342708+rajatdeveloper@users.noreply.github.com>
2018-04-21 17:32:14 +0530

    Update script.py

--- script.py
+++ script.py
@@ -1,4 +1,36 @@
-  Project - Computer Vision that creates live sketch
-	Rajat Singhal : singhal.rajat97@gmail.com		
-	B.Tech 4th Semester, Computer Science & Engineering
-	
+"""Project - Computer Vision that creates live sketch.
+   Rajat Singhal : singhal.rajat97@gmail.com
+   B.Tech 4th Semester, Computer Science & Engineering"""
+
+import numpy as np
+import cv2
+#sketch generating function
+def sketch(image):
+    #convert image to grayscale
+    img_gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+
+    #clean up image using Gaussian Blur
+    img_gray_blur =cv2.GaussianBlur(img_gray,(5,5),0)
+
+    #Extract Edges
+    canny_edges = cv2.Canny(img_gray_blur,10,70)
+
+    #Do an invert binarize the image
+    ret, mask = cv2.threshold(canny_edges,70,255,cv2.THRESH_BINARY_INV)
+
+    return mask
+
+#Initialize webcam, cap is the object provided by VideoCapture
+#It contains a boolean indicating if it was sucessful (ret)
+#It also contains the image collected from webcam (frame)
+cap =cv2.VideoCapture(0)
+
+while True:
+    ret, frame =cap.read()
+    cv2.imshow('Our Live Sketcher',sketch(frame))
+    if cv2.waitKey(1)==13 :#13 is the Enter waitKey
+        break
+
+#Release camera and close windows
+cap.release()
+cv2.destroyAllWindows()
